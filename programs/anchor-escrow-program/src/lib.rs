a54f0cc049268c8bd50554068c15eab9c38a3651
Will Wolf <williamabrwolf@gmail.com>
2021-11-27 14:01:16 -0500

    attach maker to swap state

--- programs/anchor-escrow-program/src/lib.rs
+++ programs/anchor-escrow-program/src/lib.rs
@@ -14,6 +14,7 @@ pub mod anchor_escrow_program {
     ) -> ProgramResult {
         // Set properties on swap state
         let swap_state = &mut ctx.accounts.swap_state;
+        swap_state.maker = ctx.accounts.maker.key();
         swap_state.bar_coin_amount = bar_coin_amount;
         swap_state.escrow_account_bump = escrow_account_bump;
         // Transfer FooCoin's from maker's FooCoin ATA to escrow
@@ -34,7 +35,7 @@ pub mod anchor_escrow_program {
 #[derive(Accounts)]
 #[instruction(escrow_account_bump: u8)]
 pub struct Submit<'info> {
-    #[account(init, payer = maker, space = 8 + 8 + 1)]
+    #[account(init, payer = maker, space = 8 + 32 + 8 + 1)]
     pub swap_state: Account<'info, SwapState>,
     #[account(mut, constraint = maker_foo_coin_token_account.mint == foo_coin_mint.key())]
     pub maker_foo_coin_token_account: Account<'info, TokenAccount>,
@@ -56,6 +57,7 @@ pub struct Submit<'info> {
 
 #[account]
 pub struct SwapState {
+    maker: Pubkey,
     bar_coin_amount: u64,
     escrow_account_bump: u8
 }

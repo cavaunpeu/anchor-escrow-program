9b635aff39b003a788d6d4652b11a922597221db
Will Wolf <williamabrwolf@gmail.com>
2021-12-02 21:17:57 -0500

    add escrow program table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import {Description, Header} from './components'
+import {Description, Header, EscrowProgram} from './components'
 
 const App = () => {
     return (
         <div className="bg-gray-800 p-8">
             <Header />
-            <Description />
+            <div className='flex justify-between'>
+                <Description />
+                <div className='flex flex-col w-3/5 h-screen justify-center'>
+                    <EscrowProgram />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components.tsx
+++ src/components.tsx
@@ -22,12 +22,106 @@ const Header = () => {
             <nav className="flex text-indigo-300">
                 <p className="text-indigo-100 text-4xl font-mono">Anchor Escrow Program</p>
                 <ul className="flex self-center justify-end flex-grow uppercase text-2xl font-mono">
-                    <li className="pl-8 hover:text-indigo-200 cursor-pointer">Code</li>
-                    <li className="pl-8 hover:text-indigo-200 cursor-pointer">Author</li>
+                    <li className="pl-8 hover:text-indigo-100 cursor-pointer">Code</li>
+                    <li className="pl-8 hover:text-indigo-100 cursor-pointer">Author</li>
                 </ul>
             </nav>
         </header>
     )
 }
 
-export { Description, Header };
+class EscrowProgram extends React.Component<any> {
+    render() {
+        return (
+            <div className='flex justify-center h-full w-full'>
+                <section className="antialiased text-gray-600 h-screen">
+                    <div className="flex flex-col justify-center h-full">
+                        <div className="w-full max-w-2xl mx-auto bg-indigo-50 shadow-2xl rounded-2xl border border-gray-200">
+                            <header className="px-5 py-4 border-b border-gray-100">
+                                <h2 className="font-semibold text-gray-800">Escrow</h2>
+                            </header>
+                            <div className="p-3">
+                                <div className="overflow-x-auto">
+                                    <table className="table-fixed w-full justify-center align-middle self-center">
+                                        <thead className="text-xs font-extrabold uppercase text-gray-400">
+                                            <tr>
+                                                <th className="p-2 whitespace-nowrap w-5/12">
+                                                    <div className="font-semibold text-left">Account</div>
+                                                </th>
+                                                <th className="p-2 whitespace-nowrap w-5/12">
+                                                    <div className="font-semibold text-left">Coin</div>
+                                                </th>
+                                                <th className="p-2 whitespace-nowrap">
+                                                    <div className="font-semibold text-left">Balance</div>
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="text-sm divide-y divide-gray-100">
+                                            <tr>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="flex items-center">
+                                                        <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"><img className="rounded-full" src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg" width="40" height="40" alt="Will"></img></div>
+                                                        <div className="font-medium text-gray-800">Will</div>
+                                                    </div>
+                                                </td>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="text-left">FooCoins</div>
+                                                </td>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="text-left font-medium text-green-500">100</div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="flex items-center">
+                                                        <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"><img className="rounded-full" src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-06.jpg" width="40" height="40" alt="Alan"></img></div>
+                                                        <div className="font-medium text-gray-800">Alan</div>
+                                                    </div>
+                                                </td>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="text-left">BarCoins</div>
+                                                </td>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="text-left font-medium text-green-500">100</div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="flex items-center">
+                                                        <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"><img className="rounded-full" src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-06.jpg" width="40" height="40" alt="Escrow"></img></div>
+                                                        <div className="font-medium text-gray-800">Escrow</div>
+                                                    </div>
+                                                </td>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="text-left">FooCoins</div>
+                                                </td>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="text-left font-medium">0</div>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
+
+class AccountBalance extends React.Component<any> {
+    render() {
+        return (
+            <div>
+                <div className='text-3xl w-1/2'>
+                    <span className='text-pink-300'>{this.props.accountName}:</span>
+                    <span className='text-indigo-100'>{this.props.balance}</span>
+                </div>
+            </div>
+        )
+    }
+}
+
+export { Description, Header, EscrowProgram };

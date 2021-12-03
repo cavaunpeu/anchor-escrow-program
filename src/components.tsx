ac9b4f97dda818b62c2df5d552e07f5cc3dbc3b6
Will Wolf <williamabrwolf@gmail.com>
2021-12-02 22:02:17 -0500

    add escrow terms and actions

--- src/components.tsx
+++ src/components.tsx
@@ -33,12 +33,12 @@ const Header = () => {
 class EscrowProgram extends React.Component<any> {
     render() {
         return (
-            <div className='flex justify-center h-full w-full'>
+            <div className='flex flex-col justify-center h-full w-full font-mono'>
                 <section className="antialiased text-gray-600 h-screen">
                     <div className="flex flex-col justify-center h-full">
                         <div className="w-full max-w-2xl mx-auto bg-indigo-50 shadow-2xl rounded-2xl border border-gray-200">
                             <header className="px-5 py-4 border-b border-gray-100">
-                                <h2 className="font-semibold text-gray-800">Escrow</h2>
+                                <h2 className="font-semibold text-gray-800">Account Balances</h2>
                             </header>
                             <div className="p-3">
                                 <div className="overflow-x-auto">
@@ -101,6 +101,79 @@ class EscrowProgram extends React.Component<any> {
                                             </tr>
                                         </tbody>
                                     </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="antialiased text-gray-600 h-screen">
+                    <div className="flex flex-col justify-center h-full">
+                        <div className="w-full max-w-2xl mx-auto bg-indigo-50 shadow-2xl rounded-2xl border border-gray-200">
+                            <header className="px-5 py-4 border-b border-gray-100">
+                                <h2 className="font-semibold text-gray-800">Escrow Terms</h2>
+                            </header>
+                            <div className="p-3">
+                                <div className="overflow-x-auto">
+                                    <table className="table-fixed w-full justify-center align-middle self-center">
+                                        <thead className="text-xs font-extrabold uppercase text-gray-400">
+                                            <tr>
+                                                <th className="p-2 whitespace-nowrap w-5/12">
+                                                    <div className="font-semibold text-left">Account</div>
+                                                </th>
+                                                <th className="p-2 whitespace-nowrap w-5/12">
+                                                    <div className="font-semibold text-left">Coin</div>
+                                                </th>
+                                                <th className="p-2 whitespace-nowrap">
+                                                    <div className="font-semibold text-left">Amount</div>
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="text-sm divide-y divide-gray-100">
+                                            <tr>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="flex items-center">
+                                                        <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"><img className="rounded-full" src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg" width="40" height="40" alt="Will"></img></div>
+                                                        <div className="font-medium text-gray-800">Will</div>
+                                                    </div>
+                                                </td>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="text-left">FooCoins</div>
+                                                </td>
+                                                <td className='p-2'>
+                                                    <input className="text-left w-full font-extrabold h-8 border-solid border-2 border-indigo-600 rounded-lg"/>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="flex items-center">
+                                                        <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"><img className="rounded-full" src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-06.jpg" width="40" height="40" alt="Alan"></img></div>
+                                                        <div className="font-medium text-gray-800">Alan</div>
+                                                    </div>
+                                                </td>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <div className="text-left">BarCoins</div>
+                                                </td>
+                                                <td className="p-2 whitespace-nowrap">
+                                                    <input className="text-left w-full font-extrabold h-8 border-solid border-2 border-indigo-600 rounded-lg"/>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="antialiased text-gray-600 h-screen">
+                    <div className="flex flex-col justify-center h-full">
+                        <div className="w-full max-w-2xl mx-auto">
+                            <div className="p-3">
+                                <div className="overflow-x-auto">
+                                    <div className='grid grid-cols-3 gap-12'>
+                                        <button className='bg-indigo-300 text-gray-800 hover:text-indigo-50 border-2 rounded-lg h-16 text-2xl'>Submit</button>
+                                        <button className='bg-indigo-300 text-gray-800 hover:text-indigo-50 border-2 rounded-lg h-16 text-2xl'>Accept</button>
+                                        <button className='bg-pink-300 text-gray-800 hover:text-indigo-50 border-2 rounded-lg h-16 text-2xl'>Reset</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>

c695e66adf6fe1e12a96fc66f651dad303d7c5c6
Will Wolf <williamabrwolf@gmail.com>
2021-12-14 20:28:21 -0600

    update button text size and flex box height

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -7,7 +7,7 @@ export const Navigation: FC = () => {
   return(
     <div className="bg-gray-800 p-8">
       <Header />
-      <div className='flex flex-row h-screen py-20'>
+      <div className='flex flex-row h-full py-20'>
         <div className='flex flex-col justify-start w-3/5 px-24'>
           <UserInterface />
         </div>

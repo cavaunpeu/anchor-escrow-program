09f415345392d68c19e7a4d54d6e890e841bb976
Will Wolf <williamabrwolf@gmail.com>
2021-12-14 19:53:35 -0600

    add links to header list buttons

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,8 +7,8 @@ const Header: FC = () => {
       <nav className="flex items-center text-indigo-300">
         <p className="text-indigo-100 text-4xl font-mono">Anchor Escrow Program</p>
         <ul className="flex items-center justify-end flex-grow uppercase text-2xl font-mono">
-          <li className="pl-8 hover:text-indigo-100 cursor-pointer">Code</li>
-          <li className="pl-8 hover:text-indigo-100 cursor-pointer">Author</li>
+          <li className="pl-8 hover:text-indigo-100 cursor-pointer"> <a href="https://github.com/cavaunpeu/anchor-escrow-program" target="_blank" rel="noreferrer">Code</a></li>
+          <li className="pl-8 hover:text-indigo-100 cursor-pointer"> <a href="https://willwolf.io/about/" target="_blank" rel="noreferrer">Author</a></li>
           <li className="pl-8 ">
             <WalletMultiButton />
           </li>

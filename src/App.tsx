0499fcfde57320f4ec1d96dadeb6c282e175733b
Will Wolf <williamabrwolf@gmail.com>
2021-12-16 20:55:16 -0600

    configure app to run on devnet

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,22 @@ import {
   WalletProvider,
   ConnectionProvider,
 } from "@solana/wallet-adapter-react";
+import { clusterApiUrl } from "@solana/web3.js";
 import { getPhantomWallet } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import config from "./config.json";
+
+// Get endpoint
+const cluster = config["cluster"];
+let endpoint: string;
+
+if (cluster === "localhost") {
+  endpoint = "http://127.0.0.1:8899";
+} else if (cluster === "devnet") {
+  endpoint = clusterApiUrl("devnet");
+}
 
 const App: FC = () => {
-  const endpoint = "http://127.0.0.1:8899";
   const wallets = [getPhantomWallet()];
 
   return (
